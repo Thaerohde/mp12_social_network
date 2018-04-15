cbd588d3e92b39579f8dfe5db841929fac32f3a3
Thaerohde <thaerohde@gmail.com>
2018-04-15 19:45:58 +0200

    add spring files

---
+++ src/main/java/refugeescode/at/mp12_social_network/Mp12SocialNetworkApplication.java
@@ -0,0 +1,12 @@
+package refugeescode.at.mp12_social_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp12SocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp12SocialNetworkApplication.class, args);
+	}
+}
